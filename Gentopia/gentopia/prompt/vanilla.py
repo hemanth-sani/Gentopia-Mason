3fb333ec19b046d39fcc386bea757e75a1b48a7f
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2024-03-09 21:13:17 -0500

    Update vanilla.py

--- Gentopia/gentopia/prompt/vanilla.py
+++ Gentopia/gentopia/prompt/vanilla.py
@@ -1,12 +1,12 @@
 from gentopia.prompt import PromptTemplate
-from gentopia import Google_Search 
+from gentopia.tools import GoogleSearch 
 
 VanillaPrompt = PromptTemplate(
     input_variables=["instruction"],
-    template=lambda args: Google_Search(args["[instruction]"])if "tell me about" in args["instruction"].lower() else args["instruction"]
+    template=lambda args: GoogleSearch(args["[instruction]"])if "tell me about" in args["instruction"].lower() else args["instruction"]
 )
 
 FewShotVanillaPrompt = PromptTemplate(
     input_variables=["instruction", "fewshot"],
-    template=lambda args: args["fewshot"] + "\n\n" + gentopia_search(args["instruction"]) if "tell me about" in args["instruction"].lower() else args["instruction"]
+    template=lambda args: args["fewshot"] + "\n\n" + GoogleSearch(args["instruction"]) if "tell me about" in args["instruction"].lower() else args["instruction"]
 )

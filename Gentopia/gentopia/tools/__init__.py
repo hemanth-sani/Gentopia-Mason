20e04b70148b3e72e7b908ffc158387ee42baead
Hemanth <81924396+hemanth-sani@users.noreply.github.com>
2024-03-09 23:33:46 -0500

    Update __init__.py

--- Gentopia/gentopia/tools/__init__.py
+++ Gentopia/gentopia/tools/__init__.py
@@ -13,7 +13,7 @@ from .gradio import *
 from .code_interpreter import PythonCodeInterpreter
 from .file_operation import WriteFile, ReadFile
 from .duckduckgo import DuckDuckGo
-from .Pdf_reader import FetchAndReadPDFTool
+from .Pdf_reader import ReadPDFTool
 
 
 def load_tools(name: str) -> BaseTool:
@@ -44,7 +44,7 @@ def load_tools(name: str) -> BaseTool:
         "search_single_paper": SearchSinglePaper,
         "search_related_paper": SearchRelatedPaper,
         "search_cite_paper": SearchCitePaper,
-        "pdf_reader": FetchAndReadPDFTool,
+        "pdf_reader": ReadPDFTool,
     }
     if name not in name2tool:
         raise NotImplementedError
